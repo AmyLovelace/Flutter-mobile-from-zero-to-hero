c35dc7e0587e1d3dcd28f701eec81641c42b41f5
amanecer <amanecer.luz.cabrera@gmail.com>
2023-10-11 17:53:22 -0300

    added variables data types

---
+++ 02_variables.dart
@@ -0,0 +1,20 @@
+void main(){
+  
+  final String name = 'Amisauria';
+  final int age = 29;
+  final bool isAlive = true;
+  final abilities =['bakes'];//tipo de dato inferido por su inicializacion
+  final sprites = <String>['ditto/front.png','ditto/back.png'];
+    
+  
+  
+  print("""
+  $name
+  $age
+  $isAlive
+  $abilities
+  $sprites
+  
+  """);
+  
+}
