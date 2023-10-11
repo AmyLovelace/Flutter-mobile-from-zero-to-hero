3271a2d45d56311de3f5f928746419233bb83251
amanecer <amanecer.luz.cabrera@gmail.com>
2023-10-11 19:11:45 -0300

    added Map exercises

---
+++ 03_maps.dart
@@ -0,0 +1,29 @@
+void main(){
+  
+  final Map<String,dynamic> pokemon = {
+    
+    'name': 'Ditto',
+    'hp': 15,
+    'isAlive':true,
+    'abilities':<String>['impostor','bakes'],
+    'sprites':{
+      1:'ditto/front.png',
+      2:'ditto/back.png'
+
+    }
+    
+  };
+  
+
+  
+  print (pokemon);
+  print('Name: ${pokemon['name'] }');
+  print('Ability: ${pokemon['abilities'] }');
+  print('Sprites: ${pokemon['sprites'] }');
+  print('Back: ${pokemon['sprites'][2] }');
+  print('Front: ${pokemon['sprites'][1] }');
+
+
+
+  
+}
