fc30e912d00e428c989ee151f75fefacbf04da3d
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-10-14 03:38:33 -0300

    abstract classes

--- 10_abstract_classes.dart
+++ 10_abstract_classes.dart
@@ -1,9 +1,21 @@
 void main(){
 
-
+  final heroin = Heroin(initialPrice:9);
+  
+ 
+  print(heroin);
+  
+  print('making a deal, im buying some${heroin.type} drugs: ${dealPills(heroin)}');
 }
 
 
+int dealPills(Drugs pill ){
+  if(pill.price < 10){
+    throw Exception('not enough money nigxx');
+  }
+  return pill.price - 10;
+  
+}
 enum DrugsType{hard,soft}
 
 abstract class Drugs{
@@ -20,8 +32,20 @@ abstract class Drugs{
   void buyDrugs(int amount);//sin implementacion solo extiende 
   
   
+ 
+}
+
+class Heroin extends Drugs{
   
   
   
-  
+  Heroin({required int initialPrice})
+    : super(price : initialPrice,type: DrugsType.hard);
+
+ 
+@override
+ void buyDrugs(int amount){
+   price -= amount;
+ }//implementacion de metodo extendido 
+
 }
